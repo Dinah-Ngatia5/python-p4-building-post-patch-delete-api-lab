518375469d6c69277644365caf5dabba3e3ca4d9
Dinah-Ngatia5 <dinah.ngatia@student.moringaschool.com>
2024-05-31 19:37:19 +0300

    Commit

--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+ 
 
 from flask import Flask, request, make_response, jsonify
 from flask_migrate import Migrate
@@ -14,22 +15,36 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
-def home():
-    return '<h1>Bakery GET-POST-PATCH-DELETE API</h1>'
 
-@app.route('/bakeries')
-def bakeries():
-    bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
-    return make_response(  bakeries,   200  )
+# Define the route to create a new baked good
+@app.route('/baked_goods', methods=['POST'])
+def create_baked_good():
+    name = request.form.get('name')
+    price = float(request.form.get('price'))
+    bakery_id = int(request.form.get('bakery_id'))
+    new_baked_good = BakedGood(name=name, price=price, bakery_id=bakery_id)
+    db.session.add(new_baked_good)
+    db.session.commit()
+    return jsonify({'message': 'Baked good created successfully'}), 201
+
+@app.route('/bakeries/<int:id>', methods=['PATCH'])
+def update_bakery(id):
+    if request.method == 'PATCH':
+        bakery = Bakery.query.get_or_404(id)
+        data = request.json
+        bakery.name = data['name']
+        db.session.commit()
+        return jsonify({'message': 'Bakery updated successfully'}), 200
+    else:
+        return jsonify({'message': 'Method not allowed'}), 405
+
+
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-
     bakery = Bakery.query.filter_by(id=id).first()
     bakery_serialized = bakery.to_dict()
     return make_response ( bakery_serialized, 200  )
-
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
     baked_goods_by_price = BakedGood.query.order_by(BakedGood.price.desc()).all()
@@ -37,7 +52,6 @@ def baked_goods_by_price():
         bg.to_dict() for bg in baked_goods_by_price
     ]
     return make_response( baked_goods_by_price_serialized, 200  )
-   
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
